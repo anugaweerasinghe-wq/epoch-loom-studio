32701198e1f90a0c30848fc18a2925f3159236b3
anugaweerasinghe-wq <anuga.weerasinghe@springfield.lk>
2026-05-30 13:05:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-import { nitro } from "nitropack/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   tanstackStart: {
