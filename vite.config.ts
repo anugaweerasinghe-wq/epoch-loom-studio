e967b9b0ded1fb0a40eebc41fc6289f015c07680
anugaweerasinghe-wq <anuga.weerasinghe@springfield.lk>
2026-05-30 12:09:25 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
-// @lovable.dev/vite-tanstack-config already includes the following — do NOT add them manually
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-import { nitro } from "nitro/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
   },
   vite: {
-    plugins: [
-      nitro({
-        preset: "vercel",
-      }),
-    ],
+    plugins: [cloudflare()],
   },
 });
